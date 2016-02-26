a63678d5a35ab642ddb7044b08bc22a3f2a6287b
Ken Wilcox <wilcoxk@medamerica.com>
2016-02-26 12:09:28 -0700

    uglify breaks angular

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
 
 gulp.task('js', function () {
   gulp.src(['app/app.js', 'app/**/*.js'])
      .pipe(concat('app.js'))
+     .pipe(uglify())
      .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('watch', ['js'], function () {
+  gulp.watch('app/**/*.js', ['js'])
 })
