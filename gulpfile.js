2ac450fbf17359704cf1d425fd92106e34361742
Ken Wilcox <wilcoxk@medamerica.com>
2016-02-26 12:17:14 -0700

    ng-annotate fixes that

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
+var ngAnnotate = require('gulp-ng-annotate')
 
 gulp.task('js', function () {
   gulp.src(['app/app.js', 'app/**/*.js'])
      .pipe(concat('app.js'))
+     .pipe(ngAnnotate())
      .pipe(uglify())
      .pipe(gulp.dest('dist/'))
 })
